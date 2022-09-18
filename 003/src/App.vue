2b6c84125d056efdec14f8c8357dda0f09afdcca
34ruby <ixia5027@gmail.com>
2022-09-18 21:01:20 +0900

    220918 Created Vuetify UI Web

---
+++ 003/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <Sidebar />
+
+    <v-main>
+      <HomeComponent />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Sidebar from './components/SideBar.vue'
+import HomeComponent from './components/HomeComponent.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Sidebar,
+    HomeComponent
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ 003/src/components/HomeComponent.vue
@@ -0,0 +1,309 @@
+
+<template>
+<!-- eslint-disable -->
+    <div class="home">
+        <v-container fluid class="mt-4">
+            <v-row>
+                <v-col cols="12" sm="3">
+                    <v-card color="#23262D" elevation="5">
+                        <v-row>
+                            <v-col cols="12" sm="6">
+                                <v-list-item three-line>
+                                    <v-list-item-content>
+                                        <div>
+                                            <v-list-item-subtitle class="subtitle-1 mb-14 grey--text">
+                                                GROUP A
+                                            </v-list-item-subtitle>
+                                            <v-icon color="grey" x-large>fas fa-users</v-icon>
+                                        </div>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                                <v-list-item three-line>
+                                    <v-list-item-content class="text-right">
+                                        <div>
+                                            <v-list-item-subtitle class="subtitle-1 grey--text">
+                                                742,824
+                                            </v-list-item-subtitle>
+                                            <v-list-item-subtitle class="grey--text" style="font-size: 11px">
+                                                CHANGE TO
+                                            </v-list-item-subtitle>
+                                            <v-list-item-subtitle class="grey--text mb-5" style="font-size 11px">
+                                                PREVIOUS MONTH
+                                            </v-list-item-subtitle>
+                                            <v-icon color="green" class="mt-2 mr-2">fas fa-sort-up</v-icon>
+                                            <span class="green-text">20,597</span><br>
+                                            <span class="green--text">(2.16%)</span>
+                                        </div>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-col>
+                <v-col cols="12" sm="3">
+                    <v-card color="#23262D" elevation="5">
+                        <v-row>
+                            <v-col cols="12" sm="6">
+                                <v-list-item three-line>
+                                    <v-list-item-content>
+                                        <div>
+                                            <v-list-item-subtitle class="subtitle-1 mb-14 grey--text">
+                                                GROUP B
+                                            </v-list-item-subtitle>
+                                            <v-icon color="grey" x-large>fas fa-users</v-icon>
+                                        </div>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                                <v-list-item three-line>
+                                    <v-list-item-content class="text-right">
+                                        <div>
+                                            <v-list-item-subtitle class="subtitle-1 grey--text">
+                                                889,112
+                                            </v-list-item-subtitle>
+                                            <v-list-item-subtitle class="grey--text" style="font-size: 11px">
+                                                CHANGE TO
+                                            </v-list-item-subtitle>
+                                            <v-list-item-subtitle class="grey--text mb-5" style="font-size 11px">
+                                                PREVIOUS MONTH
+                                            </v-list-item-subtitle>
+                                            <v-icon color="green" class="mt-2 mr-2">fas fa-sort-up</v-icon>
+                                            <span class="green-text">21,227</span><br>
+                                            <span class="green--text">(0.24%)</span>
+                                        </div>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-col>
+                <v-col cols="12" sm="3">
+                    <v-card color="#23262D" elevation="5">
+                        <v-row>
+                            <v-col cols="12" sm="6">
+                                <v-list-item three-line>
+                                    <v-list-item-content>
+                                        <div>
+                                            <v-list-item-subtitle class="subtitle-1 mb-14 grey--text">
+                                                GROUP C
+                                            </v-list-item-subtitle>
+                                            <v-icon color="grey" x-large>fas fa-users</v-icon>
+                                        </div>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                                <v-list-item three-line>
+                                    <v-list-item-content class="text-right">
+                                        <div>
+                                            <v-list-item-subtitle class="subtitle-1 grey--text">
+                                                372,105
+                                            </v-list-item-subtitle>
+                                            <v-list-item-subtitle class="grey--text" style="font-size: 11px">
+                                                CHANGE TO
+                                            </v-list-item-subtitle>
+                                            <v-list-item-subtitle class="grey--text mb-5" style="font-size 11px">
+                                                PREVIOUS MONTH
+                                            </v-list-item-subtitle>
+                                            <v-icon color="red" class="mt-2 mr-2">fas fa-sort-down</v-icon>
+                                            <span class="green-text">32,112</span><br>
+                                            <span class="red--text">(2.16%)</span>
+                                        </div>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-col>
+                <v-col cols="12" sm="3">
+                    <v-card color="#23262D" elevation="5">
+                        <v-row>
+                            <v-col cols="12" sm="6">
+                                <v-list-item three-line>
+                                    <v-list-item-content>
+                                        <div>
+                                            <v-list-item-subtitle class="subtitle-1 mb-14 grey--text">
+                                                GROUP D
+                                            </v-list-item-subtitle>
+                                            <v-icon color="grey" x-large>fas fa-users</v-icon>
+                                        </div>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                                <v-list-item three-line>
+                                    <v-list-item-content class="text-right">
+                                        <div>
+                                            <v-list-item-subtitle class="subtitle-1 grey--text">
+                                                65,209  
+                                            </v-list-item-subtitle>
+                                            <v-list-item-subtitle class="grey--text" style="font-size: 11px">
+                                                CHANGE TO
+                                            </v-list-item-subtitle>
+                                            <v-list-item-subtitle class="grey--text mb-5" style="font-size 11px">
+                                                PREVIOUS MONTH
+                                            </v-list-item-subtitle>
+                                            <v-icon color="green" class="mt-2 mr-2">fas fa-sort-up</v-icon>
+                                            <span class="green-text">8,771</span><br>
+                                            <span class="green--text">(5.24%)</span>
+                                        </div>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-col>
+                <v-col cols="12" sm="7">
+                    <v-card color="#23262D">
+                        <v-app-bar color="rgba(0,0,0,0)" flat>
+                            <v-toolbar-title class="grey--text title">Monthly Trend</v-toolbar-title>
+                            <v-spacer></v-spacer>
+                            <v-chip class="ma-2" color="grey darked-1" pill>May 8
+                                <v-icon right small color="cyan">
+                                    fas fa-chevron-circle-down
+                                </v-icon>
+                            </v-chip>
+                        </v-app-bar>
+                        <v-data-table :headers="headers" :items="desserts" class="dattab">
+                            <template v-slot:item.glutenfree="{item}">
+                                <v-simple-checkbox v-model="item.glutenfree" color="cyan"></v-simple-checkbox>
+                            </template>
+                            <template v-slot:item.action="">
+                                <v-icon small>mdi-pencil</v-icon>
+                            </template>
+                        </v-data-table>
+                    </v-card>
+                </v-col> 
+                <v-col cols="12" sm="5">
+                    <v-card color="#23262D">
+                        <v-app-bar color="rgba(0,0,0,0)" flat>
+                            <v-toolbar-title class="grey--text title">
+                                Support Tickets
+                            </v-toolbar-title>
+                        </v-app-bar>
+                        <v-list three-line class="mt-nS" color="#23262D">
+                            <v-list-item-group>
+                                <template v-for="(item) in files">
+                                    <v-list-item :key="item.title">
+                                        <v-list-item-avatar>
+                                            <img :src="item.avatar">
+                                        </v-list-item-avatar>
+                                        <template>
+                                            <v-list-item-content>
+                                                <v-list-item-title v-html="item.title"></v-list-item-title>
+                                                <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+                                            </v-list-item-content>
+                                            <v-list-item-action>
+                                                <v-btn v-text="item.action" rounded color="cyan" small></v-btn>
+                                            </v-list-item-action>
+                                        </template>
+                                    </v-list-item>
+                                </template>
+                            </v-list-item-group>
+                        </v-list>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'HomeComponent',
+    data() {
+        return {
+            desserts: [
+                {
+                    glutenfree: true,
+                    name: 'Kurosawa Ruby',
+                    email: 'ruby@ruby.com',
+                    contact: 998865142,
+                    status: 'Close',
+                },
+                {
+                    glutenfree: true,
+                    name: 'Nakagawa Nana',
+                    email: 'yukisetsuna@ruby.com',
+                    contact: 516621541,
+                    status: 'Open',
+                },
+                {
+                    glutenfree: true,
+                    name: 'Uehara Ayumu',
+                    email: 'pomu@ruby.com',
+                    contact: 112364473,
+                    status: 'Open',
+                },
+                {
+                    glutenfree: true,
+                    name: 'Sonoda Umi',
+                    email: 'lovearrow@ruby.com',
+                    contact: 744123131,
+                    status: 'Open',
+                },
+                // {
+                //     glutenfree: true,
+                //     name: '34ruby',
+                //     email: '34ruby@ruby.com',
+                //     contact: 998865142,
+                //     status: 'Open',
+                // },
+            ],
+            headers: [
+                {text: '', value: 'glutenfree'},
+                {
+                    text: 'Name',
+                    align: 'start',
+                    sortable: false,
+                    value: 'name',
+                },
+                {text: 'Email', value: 'email'},
+                {text: 'Contact', value: 'contact'},
+                {text: 'Status', value: 'status', sortable: false},
+                {text: 'Actions',  value: 'actions', sortable: false},
+            ],
+            files: [
+                {
+                    avatar: require('../../public/profile1.jpg'),
+                    action: 'Open',
+                    headline: 'Summer BBQ',
+                    subtitle: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. `,
+                    title: `<strong>Eburh</strong>`
+                },
+                {
+                    avatar: require('../../public/profile2.jpg'),
+                    action: 'Closed',
+                    headline: 'Summer BBQ',
+                    subtitle: `Contrary to popular belief, Lorem Ipsum is not simply random text.`,
+                    title: `<strong>Wene</strong>`
+                },
+                {
+                    avatar: require('../../public/profile3.jpg'),
+                    action: 'Closed',
+                    headline: 'Summer BBQ',
+                    subtitle: `The Extremes of Good and Evil`,
+                    title: `<strong>Iceth</strong>`
+                }, 
+                {
+                    avatar: require('../../public/profile4.jpg'),
+                    action: 'Closed',
+                    headline: 'Summer BBQ',
+                    subtitle: `injected humour and the like`,
+                    title: `<strong>Audreyn</strong>`
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .datatab {
+        background: #23262D !important;
+    }
+</style>
